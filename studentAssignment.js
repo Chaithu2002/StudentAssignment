8053b633bbb44342d3c08c98540c256beb6cf249
Chaithu2002 <chaithuchowdhary333@gmail.com>
2024-02-19 16:14:28 +0530

    ADDED: Detailed analysis of student results for each class

--- studentAssignment.js
+++ studentAssignment.js
@@ -183,7 +183,7 @@ let students = [
 
 let cond = true;
 while(cond){
-    let userInput = readline.question("select one from these three statements:\n 1. Take Test\n 2. Generate Result\n 3. View Students Result\n 4. display classwise result\n 5. Exit\n");
+    let userInput = readline.question("select one from these three statements:\n 1. Take Test\n 2. Generate Result\n 3. View Students Result\n 4. display classwise result\n 5. Detail Analysis of result\n 6. Exit\n");
     if(userInput == 1){
         takeTest();
         generateResult();
@@ -202,6 +202,9 @@ while(cond){
         displayClassWise();
     }
     if(userInput == 5){
+        detailAnalysisOfStudent();
+    }
+    if(userInput == 6){
         console.log("Thank you");
         cond = false;
     }
@@ -211,7 +214,7 @@ while(cond){
 // adding 3 subjects and generating random marks from 1 to 100 for each student in students
 function takeTest(){
     students.forEach((obj)=>{
-        let randomMarks = Math.round(Math.random()*100);
+        let randomMarks = Math.round(Math.random()*(100-30)+30);
         let subjects = ["maths","science","english"];
         count = 0;
         while(count<3){
@@ -262,10 +265,112 @@ function displayClassWise(){
         }
     })
 
-    //printing the details of the students class wise using console.table 
-
+    //storing the details of the students class wise in filteredClasses through filter method 
+    let filteredClasses = [];
     for(let cls of classes){
-        console.table(students.filter((obj)=> obj.Class == cls),["Roll_no","Name","Class","Gender","Tarks","Percentage"]);
+        filteredClasses.push(students.filter((obj)=> obj.Class == cls));
     }
 
+    //printing the details of the students class wise using console.table 
+
+    //console.log(filteredClasses);
+    for(let obj in filteredClasses){
+        console.table(filteredClasses[obj],["Roll_no","Name","Class","Gender","Total","Percentage"]);
+    }
+
+    return filteredClasses;
+
 }
+
+function detailAnalysisOfStudent(){
+    let resultArray = [];
+    let classes = displayClassWise();
+    let totalMarks = 0;
+    let averageTotalMarks = 0;
+    let failedStudentsCount = 0;
+    let eachClass = 0;
+
+    for(let array of classes){
+        //creating empty object for holding all the calculating properties
+        let resultClassObject = {};
+        let denominator = 0;
+        let totalStudents = 0;
+        let totalPercentage = 0;
+        array.forEach((obj)=>{
+            totalMarks += obj.Total;
+            totalPercentage += obj.Percentage;
+            denominator++;
+            totalStudents++;
+            let arr = obj.test_score;
+            for(let obj of arr){
+                if(obj.marks <= 44){
+                    failedStudentsCount++;
+                    break;
+                }
+            }
+            // passedStudentsCount++;     //calculating passed students count
+            eachClass = obj.Class;
+        });
+
+        // computing average percentage 
+        let classAveragePercent = Math.round(totalPercentage/denominator);
+        let classAverageGrade = overallGradeForEachClass(classAveragePercent);   // calling overall grade for each class function;
+        resultClassObject.overallGrade = classAverageGrade;
+
+        //computing average total marks and adding to object value
+        averageTotalMarks = Math.round(totalMarks / denominator);
+        resultClassObject.averageTotalMarks = averageTotalMarks;
+        // adding average percentage to object value
+        resultClassObject.averagePercentage = classAveragePercent;
+        // calculating failed students percentage
+        let failedStudentsPercentage = Math.round((failedStudentsCount / totalStudents) * 100);
+        // calculating passed students percentage
+        let passedStudentsPercentage = 100 - failedStudentsPercentage;
+
+        //adding failed and passed students count and percentage to object values
+        resultClassObject.failedStudentsCount = failedStudentsCount;
+        resultClassObject.passedStudentsCount = totalStudents - failedStudentsCount;
+        resultClassObject.failedStudentsPercentage = failedStudentsPercentage;
+        resultClassObject.passedStudentsPercentage = passedStudentsPercentage;
+        //adding each class value in class object 
+        resultClassObject.class = eachClass;
+        resultClassObject.totalStudents = totalStudents;
+        //pushing each class object with all the calculated values into result Array
+        resultArray.push(resultClassObject);
+
+    }
+    // console.log(resultArray);
+    console.table(resultArray,["class","totalStudents","averageTotalMarks","averagePercentage","overallGrade"]);
+    console.table(resultArray,["failedStudentsCount","passedStudentsCount","failedStudentsPercentage"]);
+    console.table(resultArray,["passedStudentsPercentage"]);
+}
+
+function overallGradeForEachClass(averagePercentage){
+    if(averagePercentage >= 80){
+        return "A+"; 
+    }
+    else if(averagePercentage >= 70){
+        return "A";
+    }
+    else if(averagePercentage >= 65){
+        return "B+";
+    }
+    else if(averagePercentage >= 60){
+        return "B";
+    }
+    else if(averagePercentage >= 55){
+        return "C+";
+    }
+    else if(averagePercentage >= 50){
+        return "C";
+    }
+    else if(averagePercentage >= 45){
+        return "D";
+    }
+    else{
+        return "F";
+    }
+}
+
+
+
